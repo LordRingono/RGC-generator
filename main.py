af275ff6f91728b212ef399a1611543d29e89cc1
LordRingono <40274235+LordRingono@users.noreply.github.com>
2022-12-16 10:12:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+import discord
+import os
+from discord.ext import *
+from random import *
+import discord.ext.commands as commands
+from discord.ui import Button, View
+import random
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix = '%', intents = intents)
+token = os.environ['DISCORD_TOKEN']
+classes = ["voleur(e)", "prêtre(esse)", "guerrier(re)", "mage", "inutile", "clerc(e?)", "roublard(e)", "barbare", "barde", "druide(esse)", "moine", "paladin(e)", "rôdeur(euse)", "ensorceleur(euse)", "occultiste"]
+@bot.event
+async def on_ready():
+  print("Bot connecté en tant que {0.user} !".format(bot))
+
+@bot.command()
+async def ping(ctx):
+   await ctx.respond("This is a button!")
+@bot.command()
+async def hello(ctx):
+  await ctx.send(random.choice(classes))
+  button = Button(label="clique moi", style=discord.ButtonStyle.primary)
+  view = View()
+  view.add_item(button)
+  await ctx.send("hi", view=view)
+  async def button_callback(interaction):
+    await interaction.response.send_message("Hey")
+  button.callback = button_callback
+@bot.command()
+async def generate(ctx):
+  race = ""
+  classe = random.choice(classes)
+  humain = Button(label="Humain", style=discord.ButtonStyle.primary)
+  elf = Button(label="Elfe", style=discord.ButtonStyle.primary)
+  nain = Button(label="Nain", style=discord.ButtonStyle.primary)
+  gnome = Button(label="Gnome", style=discord.ButtonStyle.primary)
+  view = View()
+  view.add_item(humain)
+  view.add_item(elf)
+  view.add_item(nain)
+  view.add_item(gnome)
+  await ctx.send("choisis une race", view=view)
+  async def button4_callback(interaction):
+    race = "Gnome(ide) "
+    humain.disabled = True
+    elf.disabled = True
+    nain.disabled = True
+    gnome.disabled = True
+    final = race + classe
+    await interaction.message.edit(content=final, view=view)
+   
+    await interaction.response.send_message("")
+    
+  async def button3_callback(interaction):
+    race = "Nain(e) "
+    humain.disabled = True
+    elf.disabled = True
+    nain.disabled = True
+    gnome.disabled = True
+    final = race + classe
+    await interaction.message.edit(content=final, view=view)
+   
+    await interaction.response.send_message("")
+    
+  async def button2_callback(interaction):
+    race = "Elfe "
+    humain.disabled = True
+    elf.disabled = True
+    nain.disabled = True
+    gnome.disabled = True
+    final = race + classe
+    await interaction.message.edit(content=final, view=view)
+   
+    await interaction.response.send_message("")
+    
+    
+  async def button1_callback(interaction):
+    race = "Humain(e) "
+    humain.disabled = True
+    elf.disabled = True
+    nain.disabled = True
+    gnome.disabled = True
+    final = race + classe
+    await interaction.message.edit(content=final, view=view)
+    await interaction.response.send_message("")
+  humain.callback = button1_callback
+  elf.callback = button2_callback
+  nain.callback = button3_callback
+  gnome.callback = button1_callback
+bot.run(token)
